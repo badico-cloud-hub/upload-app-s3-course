5e381ad6b84770f9410689494560a8f446bc542a
airtonsena10 <airtonsena41@gmail.com>
2022-04-28 11:10:45 -0300

    feat(src/pages): create _app page

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from '../styles/global';
+import theme from '../styles/theme';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
