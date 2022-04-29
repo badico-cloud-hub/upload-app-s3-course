db7ba89be54052490900225423b0915ac413c3a8
airtonsena10 <airtonsena41@gmail.com>
2022-04-29 10:34:57 -0300

    feat(src/pages): changes  _app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,25 @@
-import { ThemeProvider } from 'styled-components';
-
-import GlobalStyles from '../styles/global';
-import theme from '../styles/theme';
+import { ThemeProvider as ThemeProviderStyled } from 'styled-components';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import type { AppProps } from 'next/app';
+import GlobalStyles from '../styles/global';
+import themeStyled from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const theme = createTheme({
+    palette: {
+      ...themeStyled.colors
+    }
+  });
+
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyles />
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <ThemeProviderStyled theme={themeStyled}>
+          <Component {...pageProps} />
+          <GlobalStyles />
+        </ThemeProviderStyled>
+      </ThemeProvider>
+    </>
   );
 }
 
