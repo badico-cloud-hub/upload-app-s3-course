a6b73db935de41eb18fdfed907daa5b62c20d6cd
airtonsena10 <airtonsena41@gmail.com>
2022-05-03 19:44:46 -0300

    feat(src/stories) styles components storybook

---
+++ src/stories/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+
+  background: linear-gradient(269.57deg, #011c1c 12.93%, #052e2e 102.24%);
+
+  box-shadow: 0px 16px 16px 8px rgba(0, 4, 5, 0.4);
+`;
