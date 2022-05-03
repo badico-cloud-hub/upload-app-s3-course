ecc7f66fedc7c2ec6be4a387fd03d249743111ec
Edson Almeida Silva Junior <edsonjunior.1219@gmail.com>
2022-05-03 20:42:04 -0300

    chore: next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  excludeFile: str => /\*.{spec,test,stories}.tsx/.test(str),
+  webpack: (config, { dev }) => {
+    config.module.rules.push(
+      {
+        test: /\.stories.ts$/,
+        loader: 'ignore-loader',
+      }
+    );
+    return config;
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
