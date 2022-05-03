dc5049dea0d01d5bca61a1d9143d5521365e0309
Edson Almeida Silva Junior <edsonjunior.1219@gmail.com>
2022-05-03 20:43:28 -0300

    fix(pages/_document): change function

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,8 @@
-import Document, {
-  DocumentContext,
-  DocumentInitialProps,
-  Head,
-  Html,
-  Main,
-  NextScript
-} from 'next/document';
-
+import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
@@ -35,29 +25,5 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
-  }
-
-  render() {
-    return (
-      <Html lang="pt-br">
-        <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-            rel="stylesheet"
-          />
-
-          <link rel="ico" href="https://rocketseat.com.br/favicon.ico" />
-
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
   }
 }
