c02898229a4a7e849c83cfc54cb8e0da9ccdf696
airtonsena10 <airtonsena41@gmail.com>
2022-05-23 14:48:43 -0300

    feat:(src/Components/Header)  style change

--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.header`
   width: 100%;
 
-  background: linear-gradient(269.57deg, #011c1c 12.93%, #052e2e 102.24%);
+ background: linear-gradient(89.62deg, #014154 -63.72%, #000000 27.66%, #00080D 111.86%);
 
   box-shadow: 0px 16px 16px 8px rgba(0, 4, 5, 0.4);
 `;
