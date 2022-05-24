31e2820d9cf527bcc0c4e0950d6cf97c598dd531
airtonsena10 <airtonsena41@gmail.com>
2022-05-24 14:48:59 -0300

    feat(srcComponents/upload)  add data mocke

---
+++ src/Components/upload/mocke.ts
@@ -0,0 +1,19 @@
+const mocke = (
+[
+    {
+        "_id": "628cc426a5323696aae6e722",
+        "name": "patoverde.png",
+        "__v": 0,
+        "preview": "https://images.emojiterra.com/google/android-11/512px/1f986.png"
+    },
+    {
+        "_id": "628cc6bba5323696aae6e725",
+        "name": "pato.jpg",
+        "__v": 0,
+        "preview": "https://1.bp.blogspot.com/-mJUY6abDfpg/Xbby24jlmXI/AAAAAAAAK8k/SdlL1wMq5-kX5OnuE6FDQ8Wqr5fGxgZHQCLcBGAsYHQ/s320/pato.jpg"
+    }
+]
+
+)
+
+export default mocke;
